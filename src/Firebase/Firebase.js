2d22610a0964c8d47a20773c40cc175e656a4f40
Pushkar <163520919+Pu5hk4r@users.noreply.github.com>
2025-05-07 13:55:21 +0530

    Remove Firebase.js

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -6,12 +6,12 @@ import "firebase/storage";
 const provider = new firebase.auth.GoogleAuthProvider();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDf5eGOv-0VgTxUPbOlZfQLlq9AICcFktk",
-  authDomain: "chatify-pushkar.firebaseapp.com",
-  projectId: "chatify-pushkar",
-  storageBucket: "chatify-pushkar.firebasestorage.app",
-  messagingSenderId: "18548285624",
-  appId: "1:18548285624:web:09596edaf1c25a6f95944d"
+  apiKey: 
+  authDomain: 
+  projectId: 
+  storageBucket: 
+  messagingSenderId: 
+  appId: 
 };
 
 
